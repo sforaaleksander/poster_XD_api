ea7810d1b299a4589a00aa1f542d4ce9a22707bd
MateuszGolda <mateusz.golda1993@gmail.com>
2020-09-14 18:56:18 +0200

    extract persistObjects method

--- src/main/java/com/codecool/rest_api/App.java
+++ src/main/java/com/codecool/rest_api/App.java
@@ -28,7 +28,7 @@ public class App {
         Location cracow = new Location("Cracow", 50.096898F, 19.936306F);
         Location warsaw = new Location("Warsaw", 52.229675F, 21.012230F);
         Location budapest = new Location("Budapest", 44.426765F, 26.102537F);
-        
+
         User jan = new User("Jan", "Kowalski", "123", "j@gmail.com", true);
         User balazs = new User("Balázs", "Faragó", "123", "b@gmail.com", true);
         User matthew = new User("Matthew", "Golda", "123", "m@gmail.com", true);
@@ -36,22 +36,12 @@ public class App {
         Post mPost1 = new Post(matthew, cracow, dp.parseDate("2020-09-01"), "some content");
         Post mPost2 = new Post(matthew, warsaw, dp.parseDate("2020-09-02"), "some content2");
         Comment mComment1 = new Comment(mPost1, dp.parseDate("2020-09-05"), jan, "nice content");
-        Comment mComment2 = new Comment(mPost1, dp.parseDate("2020-09-05"), balazs, "test");
-        Comment mComment3 = new Comment(mPost1, dp.parseDate("2020-09-05"), matthew, "comment content");
+        Comment mComment2 = new Comment(mPost1, dp.parseDate("2020-09-06"), balazs, "test");
+        Comment mComment3 = new Comment(mPost1, dp.parseDate("2020-09-06"), matthew, "comment content");
 
         EntityTransaction transaction = em.getTransaction();
         transaction.begin();
-        em.persist(mPost1);
-        em.persist(mPost2);
-        em.persist(cracow);
-        em.persist(warsaw);
-        em.persist(budapest);
-        em.persist(jan);
-        em.persist(balazs);
-        em.persist(matthew);
-        em.persist(mComment1);
-        em.persist(mComment2);
-        em.persist(mComment3);
+        persistObjects(em, mPost1, mPost2, cracow, warsaw, budapest, jan, balazs, matthew, mComment1, mComment2, mComment3);
         transaction.commit();
         System.out.println("\n### Matthew saved.\n");
 
@@ -60,10 +50,14 @@ public class App {
         Comment comment2 = new Comment(aPost, dp.parseDate("2020-09-09"), matthew, "test comment");
 
         transaction.begin();
-        em.persist(alek);
-        em.persist(aPost);
-        em.persist(comment2);
+        persistObjects(em, alek, aPost, comment2);
         transaction.commit();
         System.out.println("\n### Alek saved.\n");
+    }
+
+    static void persistObjects(EntityManager em, Object... objects) {
+        for (Object object : objects) {
+            em.persist(object);
+        }
     }
 }
