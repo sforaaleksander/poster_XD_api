c53dcda201fd1b07394f81dcb2782e7a3ac5442c
sforaaleksander <sforaaleksander@gmail.com>
2020-09-15 13:48:55 +0200

    debugging db

--- src/main/java/com/codecool/rest_api/servlets/PostServlet.java
+++ src/main/java/com/codecool/rest_api/servlets/PostServlet.java
@@ -1,5 +1,6 @@
 package com.codecool.rest_api.servlets;
 
+import com.codecool.rest_api.dao.PostDAO;
 import com.codecool.rest_api.models.Post;
 
 import javax.persistence.EntityManager;
@@ -14,27 +15,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 
 @WebServlet(name = "posts", urlPatterns = {"/posts/*"})
 public class PostServlet extends HttpServlet {
 
-    private EntityManager entityManager;
+    private final PostDAO postDAO = new PostDAO();
 
-    public PostServlet() {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaexamplePU");
-        this.entityManager = emf.createEntityManager();
-    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        String postId = req.getPathInfo().replace("/", "");
         List<String> elements = Arrays.stream(req.getRequestURI().split("/")).filter(e -> !e.equals("")).collect(Collectors.toList());
-//        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaexamplePU");
-//        EntityManager em = emf.createEntityManager();
-//        Post post = em.find(Post.class, elements.get(elements.size()-1));
-//        Post post = findPost(Integer.parseInt(elements.get(elements.size()-1)));
+//        Optional<Post> optionalPost = postDAO.getById((long) 1);
+//        Post post = optionalPost.get();
         PrintWriter out = resp.getWriter();
 //        out.println(post.toString());
 //        elements.forEach(out::println);
@@ -50,17 +46,5 @@ public class PostServlet extends HttpServlet {
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doDelete(req, resp);
-    }
-
-    public Post findPost(int id) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaexamplePU");
-        EntityManager em = emf.createEntityManager();
-
-        em.clear();
-
-        Post post = em.find(Post.class, id);
-        em.close();
-        emf.close();
-        return post;
     }
 }
