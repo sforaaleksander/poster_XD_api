af3b068753a0f0e446e6a653d2ce157b03d55241
sforaaleksander <sforaaleksander@gmail.com>
2020-09-17 15:41:54 +0200

    implement getObjectsForRoot() in LocationServlet.java

--- src/main/java/com/codecool/poster_xd_api/servlets/LocationServlet.java
+++ src/main/java/com/codecool/poster_xd_api/servlets/LocationServlet.java
@@ -1,13 +1,20 @@
 package com.codecool.poster_xd_api.servlets;
 
 import com.codecool.poster_xd_api.dao.LocationDao;
+import com.codecool.poster_xd_api.models.Jsonable;
 import com.codecool.poster_xd_api.models.Location;
 import com.codecool.poster_xd_api.models.Post;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @WebServlet(name = "locations", urlPatterns = {"/locations/*"}, loadOnStartup = 1)
 public class LocationServlet extends PosterAbstractServlet<Location, Post> {
@@ -30,7 +37,6 @@ public class LocationServlet extends PosterAbstractServlet<Location, Post> {
         String name = nameJson.getAsString();
         float latitude = latitudeJson.getAsFloat();
         float longitude = longitudeJson.getAsFloat();
-
         return Optional.of(new Location(name, latitude, longitude));
     }
 
@@ -38,7 +44,16 @@ public class LocationServlet extends PosterAbstractServlet<Location, Post> {
     protected void updateObject(JsonObject requestAsJson, Location location) {
         JsonElement name = requestAsJson.get("name");
         if (name != null) location.setName(name.getAsString());
-
         dao.update(location);
+    }
+
+    @Override
+    protected void getObjectsForRoot(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        List<Location> locationList;
+        List<List<Location>> lists = new ArrayList<>();
+        addObjectsMatchingParameter(req, lists, "name");
+        locationList = populateObjectList(lists);
+        String objectsAsJsonString = locationList.stream().map(e->(Jsonable)e).map(Jsonable::toJson).collect(Collectors.joining(",\n"));
+        writeObjectsToResponseFromCollection(resp, objectsAsJsonString);
     }
 }
