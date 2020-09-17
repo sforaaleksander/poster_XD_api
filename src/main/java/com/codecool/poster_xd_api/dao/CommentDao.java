1252c977b630664009503b569a1e6fe0918eb81c
sforaaleksander <sforaaleksander@gmail.com>
2020-09-17 18:05:11 +0200

    remove unused imports from Dao classes

--- src/main/java/com/codecool/poster_xd_api/dao/CommentDao.java
+++ src/main/java/com/codecool/poster_xd_api/dao/CommentDao.java
@@ -1,9 +1,6 @@
 package com.codecool.poster_xd_api.dao;
 
 import com.codecool.poster_xd_api.models.Comment;
-import com.codecool.poster_xd_api.models.User;
-
-import java.util.List;
 
 public class CommentDao extends AbstractDao<Comment>{
 
--- src/main/java/com/codecool/poster_xd_api/dao/LocationDao.java
+++ src/main/java/com/codecool/poster_xd_api/dao/LocationDao.java
@@ -1,9 +1,6 @@
 package com.codecool.poster_xd_api.dao;
 
 import com.codecool.poster_xd_api.models.Location;
-import com.codecool.poster_xd_api.models.User;
-
-import java.util.List;
 
 public class LocationDao extends AbstractDao<Location> {
 
